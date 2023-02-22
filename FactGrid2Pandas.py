2284f0aabd2be776317c2a9064152adcb233db14
MichaelMarkert <101104547+MichaelMarkert@users.noreply.github.com>
2023-02-22 19:16:23 +0100

    Update FactGrid2Pandas.py

--- FactGrid2Pandas.py
+++ FactGrid2Pandas.py
@@ -3,7 +3,8 @@ from IPython.display import display
 from pandas import json_normalize
 import re
 import locale, datetime
-locale.setlocale(locale.LC_TIME, locale.normalize("de"))
+#locale.setlocale(locale.LC_TIME, locale.normalize("de"))
+locale.setlocale(locale.LC_ALL, "de_DE")
 
 import streamlit as st
 st.set_page_config(layout="wide")
